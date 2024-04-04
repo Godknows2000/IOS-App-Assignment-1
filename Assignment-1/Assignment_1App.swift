76a1f442f6c917d2e233b0e219833eb95253430b
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2024-04-04 09:32:47 +0200

    Initial Commit

---
+++ Assignment-1/Assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1App.swift
+//  Assignment-1
+//
+//  Created by IACD Training 10 on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
